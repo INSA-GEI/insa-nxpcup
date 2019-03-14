5211ff7282b23d1ae35fd5446d13d131ab7f502c
jeje98751 <turi@etud.insa-toulouse.fr>
2019-03-14 16:51:30 +0100

    add comments

--- detection.c
+++ detection.c
@@ -6,3 +6,38 @@
  */
 
 
+void update_corrector_type(int *nbr_detection_s, int* nbr_detection_c, int MODE, int *type)
+{
+	if (MODE==STRAIGHT)
+	{
+		if (*nbr_detection_s<=100)
+		{
+			*nbr_detection_s+=1;
+		}
+		if (*nbr_detection_c>=0)
+		{
+			*nbr_detection_c-=1;
+		}
+	}
+	
+	else if (MODE==CURVE)
+	{
+		if (*nbr_detection_c<=100)
+				{
+					*nbr_detection_c+=1;
+				}
+				if (*nbr_detection_s>=0)
+				{
+					*nbr_detection_s-=1;
+				}
+	}
+	
+	if (*nbr_detection_straight_s>50)
+	{
+		*type=STRAIGHT;
+	}
+	if (*nbr_detection_c>80)
+	{
+		*type=CURVE;
+	}
+}
--- detection.h
+++ detection.h
@@ -8,6 +8,26 @@
 #ifndef DETECTION_H_
 #define DETECTION_H_
 
+//MODE for update_detection_type
+#define STRAIGHT					1
+#define CURVE						2
 
+void update_corrector_type(int *nbr_detection_s, int* nbr_detection_c, int MODE)
+
+/*ADD 3 variables in MAIN:
+int *nbr_detection_straight;
+int *nbr_detection_curve;
+int *type_detection
+
+in the init add this code:
+*nbr_detection_straight=100;
+*nbr_detection_curve=50;
+*type=STRAIGHT;
+
+use "type" as a variable to choose the corrector depending on the case;
+
+use update_corrector_type in image_processing algorithm
+
+*/
 
 #endif /* DETECTION_H_ */
