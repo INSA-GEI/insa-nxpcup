1a73a904af8db363daf62b7002d612d06393c8db
Thomas Zennaro <thomas.zennaro.1@ens.etsmtl.ca>
2020-01-23 12:59:22 +0100

    Config encoders

--- code/2019-2020/periphs/Encoder/Sources/Encoder.h
+++ code/2019-2020/periphs/Encoder/Sources/Encoder.h
@@ -1,14 +1,26 @@
 /*
- * Encoder.h
+ * encoder.h
  *
- *  Created on: Dec 28, 2019
- *      Author: User
+ *  Created on: Jan 23, 2020
+ *      Author: thomas
  */
 
 #ifndef ENCODER_H_
 #define ENCODER_H_
 
 #include <MKL25Z4.h>
+
+#define ENCODER_MASK_TPM2_PRESCALER 3 	//temporary value...
+#define ENCODER_ARR 4096				//temporary value...
+
+#define SET_CHANNEL	0x04			//Set these bits to configure channel 0 on "Input Capture" 
+										//with "Capture on Rising Edge Only" configuration
+
+
+
+/* This function initialises both encoders*/
+void encoder_config(void);
+
 
 
 #endif /* ENCODER_H_ */
