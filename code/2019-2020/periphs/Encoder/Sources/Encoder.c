be116a2b4846009dd5f48da9016add937e16ce97
reivax-boucoi <reivax.boucoi@laposte.net>
2020-01-28 21:04:04 +0100

    -mupdates encoder calculations and arr values

--- code/2019-2020/periphs/Encoder/Sources/Encoder.c
+++ code/2019-2020/periphs/Encoder/Sources/Encoder.c
@@ -10,7 +10,7 @@
 
 int state=0;
 
-void encoder_config(void){
+void encoder_init(void){
 	SIM_SCGC5 = SIM_SCGC5_PORTA_MASK | SIM_SCGC5_PORTB_MASK | SIM_SCGC5_PORTD_MASK; //Enable the clock of PORTA, PORTB, PORTD
 	SIM_SCGC6 |= SIM_SCGC6_TPM2_MASK; //Enable the clock of TPM2 
 	
@@ -24,21 +24,10 @@ void encoder_config(void){
 	
 	//TPM2_C0V = ;								// To complete...
 	
-	//Not sure for the value in PORT_PCR_MUX()...
-<<<<<<< HEAD
-	//PORTA_PCR13 |= PORT_PCR_MUX(1);				// ENC_INDEX_1 PTA13 
-	//PORTD_PCR4 |= PORT_PCR_MUX(1);				// ENC_INDEX_2 PTD4 
-=======
->>>>>>> 69da52f1fb5650bf0b12ea4fdbac7fda48b4a6b0
 	PORTB_PCR2 |= PORT_PCR_MUX(3);				// ENC_SIG_A1 PT TPM2_CH0
 	PORTB_PCR3 |= PORT_PCR_MUX(3);				// ENC_SIG_A2 PTA12 TPM2_CH1
 	
 	//Configures the individual port pins for input or output
-<<<<<<< HEAD
-	//GPIOA_PDDR |= (1<<13);
-	//GPIOD_PDDR |= (1<<4);
-=======
->>>>>>> 69da52f1fb5650bf0b12ea4fdbac7fda48b4a6b0
 	GPIOB_PDDR |= (1<<2);
 	GPIOB_PDDR |= (1<<3); 
 	
@@ -51,16 +40,10 @@ void encoder_config(void){
 
 
 void FTM2_IRQHandler() {
-<<<<<<< HEAD
-	//Clear the bit of the interrupt FTM2
-	TPM2_SC |= TPM_SC_TOF_MASK;
-=======
 	
 	int prev_ccr1 = 0;
 	int prev_ccr2 = 0;
 	int delta = 0;
-	
-	
 	
 	//Clear the bit flag of the overflow interrupt FTM2
 	if (!(TPM2_SC & TPM_SC_TOF_MASK)) {
@@ -89,7 +72,5 @@ void FTM2_IRQHandler() {
 			prev_ccr2 = TPM2_C1V;
 	}
 	
-	
->>>>>>> 69da52f1fb5650bf0b12ea4fdbac7fda48b4a6b0
 }
 
--- code/2019-2020/periphs/Encoder/Sources/Encoder.h
+++ code/2019-2020/periphs/Encoder/Sources/Encoder.h
@@ -10,15 +10,19 @@
 
 #include <MKL25Z4.h>
 
-#define RAY 3.25*0.01					//in meter
+#define WHEEL_DIAMETER 6.5*0.01					//in meter
 #define PI 3.1415926535					//						
-#define IMPULSE_TIME	2*PI*RAY/360	// =2*Pi*R/360 s	due to :
-										// 360 fronts/tr
-										// 360/(2*Pi*R) fronts/m
-										// 360/(2*Pi*R) fronts/m.s
+#define IMPULSE_TIME	PI*WHEEL_DIAMETER/360	// =Pi*D/360 s	due to :
+												// 360 fronts/tr
+												// 360/(Pi*D) fronts/m
+												// 360/(Pi*D) fronts/m.s
+
+//if max speed = 2m/s <=> Pulses/s=speed*360/(Pi*D)=3526 pulses/s
+//Resolution souhaitee : 2000pts/65535pts tot @ 2m/s d'ou PSC=2000*Fcpu/(3526*2^16)=207 -> max is 128
+// Res @ PSC=128 : Cnt=Fcpu/(PSC*3526)=53 pulses @ 2m/s
 
 #define ENCODER_MASK_TPM2_PRESCALER 7 	//Divides the clock by 128
-#define ENCODER_ARR 5000				//temporary value...
+#define ENCODER_ARR 65535
 
 #define SET_CHANNEL	0x04			//Set these bits to configure channel 0 on "Input Capture" 
 										//with "Capture on Rising Edge Only" configuration
@@ -26,7 +30,7 @@
 
 
 /* This function initialises both encoders*/
-void encoder_config(void);
+void encoder_init(void);
 
 
 
