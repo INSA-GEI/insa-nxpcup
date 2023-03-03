1a1732e7b49a06c1e107c5ebc6e28462eb0bf03c
vezde <dimercur@insa-toulouse.fr>
2023-03-03 16:19:21 +0100

    Portage du projet 2019-2020/Equipe2/NXP_Dry sous mcuexpresso (projet 2019-2020/Equipe2/NXP-DRY-2023)

---
+++ code/2019-2020/Equipe2/NXP-DRY-2023/application/Motor.c
@@ -0,0 +1,51 @@
+/*
+ * Motor.c
+ *
+ *  Created on: Dec 7, 2019
+ *      Author: User
+ */
+
+#include "Motor.h"
+
+void motor_init(void){
+
+	SIM->SCGC6 |= SIM_SCGC6_TPM0_MASK | SIM_SCGC6_TPM1_MASK | SIM_SCGC6_TPM2_MASK;
+	SIM->SCGC5 = SIM_SCGC5_PORTA_MASK | SIM_SCGC5_PORTC_MASK;
+
+	//should be right by clock_init
+	//SIM_SOPT2 &= ~SIM_SOPT2_TPMSRC_MASK;
+	//SIM_SOPT2 |= SIM_SOPT2_TPMSRC(1);
+
+	TPM0->SC |= 3; 					// prescaler
+	TPM0->MOD = 600; 				// frequency modulo
+	TPM0->SC |= TPM_SC_CMOD(1); 		// enable timer
+	TPM0->CONTROLS[1].CnSC = 0x28;				// CH1 (right  motor)
+	TPM0->CONTROLS[5].CnSC = 0x28;				// CH2 (left motor)
+
+	PORTA->PCR[4]   = 0;
+	PORTA->PCR[4]  |= PORT_PCR_MUX(3);	// PTA4 Motor Right In 1 (speed) PTA4 TPM0_CH1
+	PORTA->PCR[5]  |= PORT_PCR_MUX(1);	// PTA5 Motor Right In 2 (direction)
+	PORTC->PCR[8]  |= PORT_PCR_MUX(1);	// PTC8 Motor Left  In 1 (direction)
+	PORTC->PCR[9]  |= PORT_PCR_MUX(3);	// PTC9 Motor Left  In 2 (speed) PTC9 TPM0_CH5
+
+
+	PORTC->PCR[7]  |= PORT_PCR_MUX(1);	// Motor Right Enable
+	PORTC->PCR[0]  |= PORT_PCR_MUX(1);	// Motor Left  Enable
+
+	GPIOA->PDDR |= (1<<4);
+	GPIOA->PDDR |= (1<<5);
+	GPIOC->PDDR |= (1<<8);
+	GPIOC->PDDR |= (1<<9);
+	GPIOC->PDDR |= (1<<0);
+	GPIOC->PDDR |= (1<<7);
+
+	MOTOR_LEFT_ENABLE;
+	MOTOR_RIGHT_ENABLE;
+
+	MOTOR_LEFT_FORWARD;
+	MOTOR_RIGHT_FORWARD;
+
+	MOTOR_LEFT_FSPEED(0);
+	MOTOR_RIGHT_FSPEED(0);
+
+}
---
+++ code/2019-2020/Equipe2/NXP-DRY-2023/application/servo.h
@@ -0,0 +1,38 @@
+/*
+ * servo.h
+ *
+ *  Created on: Dec 8, 2019
+ *      Author: thomas
+ */
+
+#ifndef SERVO_H_
+#define SERVO_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <MKL25Z4.h>
+
+
+#define SERVO_MASK_TPM1_PRESCALER 3	//3=(11)2 => clock divided by 8 (see datasheet p553)
+#define SERVO_TPM1_ARR 60000			//Value of the auto-reload of the TIMER
+
+#define SERVO_SET_CHANNEL_0 0x28 		//Set these bits to configure channel 0 on "Edge-aligned PWM" with "high true pulses" configuration
+
+#define  SERVO_CENTER_POS 7454		// initial servo position (center)
+#define SERVO_HARD_LEFT 8579
+#define SERVO_HARD_RIGHT 6373
+
+#define SERVO_MAX_LEFT_ANGLE -40.0
+#define SERVO_MAX_RIGHT_ANGLE 54.5
+//Configuration of the pins/clock
+void servo_init(void);
+
+void servo_setPos(int angle);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SERVO_H_ */
