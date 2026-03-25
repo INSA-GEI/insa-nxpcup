af0d4f051187133030bdbbe2f2f72cab3dd4db5e
DI MERCURIO Sebastien <dimercur@insa-toulouse.fr>
2026-03-25 10:16:06 +0100

    Creation d'un projet car-control-frdmmcxn236 créé "from scratch" avec la HAL a jour et dans lequel les fichiers de gestion camera et ecran du projet dm-mcxn236-camera-interface-by-smartdma ont été importé. Pas de freertos pour l'instant

---
+++ code/2025-2026/car_control_frdmmcxn236/application/app.h
@@ -0,0 +1,15 @@
+/*
+ * app.h
+ *
+ *  Created on: 25 mars 2026
+ *      Author: dimercur
+ */
+
+#ifndef APP_H_
+#define APP_H_
+
+void APP_Init(void);
+
+void APP_Run(void);
+
+#endif /* APP_H_ */
